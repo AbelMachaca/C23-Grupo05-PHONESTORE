18a8ea1b0c0912435aaa8baf9640ee2d68270fa9
EmmanuelCrocianelli <maguiteamoyoema@gmail.com>
2023-11-10 18:46:34 -0300

    se levanto servidor y se creo archivo home.html

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+
+
+
+
+
+
+
 
 app.listen(3030,()=>console.log("Levantando un servidor 3030"))
