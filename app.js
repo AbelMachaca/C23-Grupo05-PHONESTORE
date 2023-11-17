27ec3b8eea1708c59f49ecfe0dba97b4a0016ee6
Jorge Huanca <jorgeh_98_jc@hotmail.com>
2023-11-17 20:07:33 -0300

    productCart.html y carrito.css en desarrollo

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"))
+});
 
 
 
