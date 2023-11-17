fa44b1826ee38dcd499752a5fc2df24ca93b0f8c
serincubus <serincubus2@gmail.com>
2023-11-17 02:40:50 -0300

    app.js modificado y directorios relacionados con productDetail

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static("public"));
 
+
+app.get("/productDetail.html", (req,res)=>{
+ res.sendFile(path.join(__dirname, "/views/productDetail.html"))    
+ 
+})
+
+
+app.use(express.static("public"));
 app.listen(3030,()=>console.log("Levantando un servidor 3030"))
