6d8af5c41d3183326b04c4b80b05418931b1f15a
Abel Machaca <abelmachaca99@gmail.com>
2023-11-20 22:12:39 -0300

    corrigiendo errores

--- app.js
+++ app.js
@@ -5,20 +5,19 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
 app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html"))
-});
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+  });
 
 
 
+  
 
-
-
-
+app.use(express.static("public"));
 app.listen(3030,()=>console.log("Levantando un servidor 3030"))
