8f34047c930d71fa9585c4dd97e8279c13bc77b6
Abel Machaca <abelmachaca99@gmail.com>
2023-11-28 00:17:44 -0300

    guardando cambios

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+/*
+app.use(express.static(path.join(__dirname, 'public')));*/
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
@@ -22,5 +24,4 @@ app.get('/registro', (req, res) => {
 
 
 
-app.use(express.static("public"));
 app.listen(3030,()=>console.log("Levantando un servidor 3030"))
