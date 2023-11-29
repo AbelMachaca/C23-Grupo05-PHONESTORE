55e92a1839f76517a576190bd40f0285172c2475
Lucas Villalba <lucasjaviervillalba@gmail.com>
2023-11-28 23:09:40 -0300

    cambie de carpeta las imagenes

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-
-
-
-
-
-
-const express = require('express');
-const app = express();
-const path = require('path');
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
-/*
-app.use(express.static(path.join(__dirname, 'public')));*/
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
@@ -26,18 +14,12 @@ app.get('/carrito', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/productCart.html"))
 });
 
-app.get('/ingresa', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html"))
-});
 
-app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'register.html'));
-  });
 
-  app.get("/productDetail", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/productDetail.html"))    
- 
-})
+
+
+
+
 
 
 app.listen(3030,()=>console.log("Levantando un servidor 3030"))
