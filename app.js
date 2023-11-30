9214581ab99f6f2b22eca4ed4a7427eaa3aedd65
serincubus <serincubus2@gmail.com>
2023-11-29 21:12:36 -0300

    agregue enlace a logo header en productDetail y productCart

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
 });
-app.get('/carrito', (req, res) => {
+app.get('/productCart', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/productCart.html"))
 });
 
-app.get('/ingresa', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
-app.get('/registro', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'register.html'));
   });
 
