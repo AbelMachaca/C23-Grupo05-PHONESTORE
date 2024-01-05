597b01398cd2dfe8f74e6ad475fc92e429b0824e
Lucas Villalba <lucasjaviervillalba@gmail.com>
2024-01-04 21:53:47 -0300

    probando crear producto

--- phonestore_2.0/src/controllers/productController.js
+++ phonestore_2.0/src/controllers/productController.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const productsFilePath = path.join(__dirname, "../data/products.json");
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require("uuid");
 
 const getJson = () => {
   const productsFilePath = path.join(__dirname, "../data/products.json");
@@ -10,8 +9,6 @@ const getJson = () => {
   return products;
 };
 const productController = {
- 
-  
   cart: (req, res) => {
     res.render("products/productCart");
   },
@@ -26,35 +23,8 @@ const productController = {
     res.render("products/productCreate_form");
   },
 
-  /*store: (req, res) => {
-		const {name,price,discount,category,description} = req.body;
-		const products = getJson();
-		if(!req.files){
-			const error = new Error("Por favor seleccione un archivo");
-			error.httpStatusCode = 400;
-			return next(error)
-		}
-    const images = req.files.map(element => element.filename);
-
-		console.log(req.files);
-		const newProduct = {
-			id:uuidv4(),
-			name:name.trim(),
-      description,
-      category,
-			price,
-			discount,		
-			description:description.trim(),
-      color,
-      price:price.trim(),
-      image: images.length > 0 ? images[0] : null,		}
-		products.push(newProduct);
-		const json = JSON.stringify(products);
-		fs.writeFileSync(productsFilePath,json,"utf-8");
-		res.redirect(`/`)
-	},*/
   store: (req, res) => {
-    const { name, category,price ,description } = req.body;
+    const { name, category, price, description } = req.body;
     const products = getJson();
 
     if (!req.file) {
@@ -63,19 +33,17 @@ const productController = {
       return res.status(400).send(error.message);
     }
 
-    const image = req.file.filename; 
+    const image = req.file.filename;
 
     console.log(req.file);
-    
+
     const newProduct = {
       id: uuidv4(),
       name: name.trim(),
-      image:image,
+      image: image,
       category,
       price: price.trim(),
-     
-      
-   
+
       description: description.trim(),
     };
 
@@ -84,7 +52,7 @@ const productController = {
     fs.writeFileSync(productsFilePath, json, "utf-8");
     res.redirect("/");
   },
- 
+
   dashboard: (req, res) => {
     res.render("products/dashboard");
   },
