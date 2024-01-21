32474ed269d1d2d5ce117c567c79bdc88fffca1d
Abel Machaca <abelmachaca99@gmail.com>
2024-01-21 15:33:25 -0300

    se agrego el registro de usuarios

--- phonestore_2.0/src/routes/users.js
+++ phonestore_2.0/src/routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const uploadFile = require('../validations/ImageUploader');
+const registerValidation = require('../validations/validationRegister');
+
 
 /* GET users listing. */
 router.get('/login', userController.login);
-router.get('/register', userController.register);
+
+router.get('/register', userController.formRegister);
+router.post('/register', uploadFile.single('image'), registerValidation, userController.register);
 
 module.exports = router;
