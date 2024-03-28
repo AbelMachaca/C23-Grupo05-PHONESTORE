6f5c5eef4197ce9d296d591140992c532d3e5b3f
wrs0809 <145084286+wrs0809@users.noreply.github.com>
2024-03-28 00:24:29 -0300

    actulizacion en imagen profile

--- phonestore_2.0/src/routes/users.js
+++ phonestore_2.0/src/routes/users.js
@@ -51,7 +51,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null,path.join(__dirname,"../","../public/images/usersProfile"))
+        cb(null,path.join(__dirname,"../","../public/images/users"))
 },
     filename:(req, file, cb) => {
         cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
