b38f9076432ded5866a425ec57e1242fb757e0a3
Abel Machaca <abelmachaca99@gmail.com>
2024-04-01 22:38:42 -0300

    validaciones del frontend del formulario de registro

--- phonestore_2.0/src/validations/validationRegister.js
+++ phonestore_2.0/src/validations/validationRegister.js
@@ -1,6 +1,7 @@
-const { body } = require('express-validator');
+
 const fs = require('fs');
 const db = require("../database/models")
+const { body, validationResult } = require('express-validator');
 
 
 const getJson = (fileName) => {
@@ -18,6 +19,13 @@ module.exports = [
   body('apellido')
     .notEmpty().withMessage('El campo no puede estar vacío').bail()
     .isLength({ min: 3, max: 30 }).withMessage('El valor ingresado debe tener al menos 3 caracteres y máximo 30').bail(),
+    body('imagen_usuario')
+    .custom((value, { req }) => {
+      if (req.errorValidationImage) {
+          return false;
+      };
+      return true;
+  }).withMessage("Esta imagen no tiene un formato válido"),
   body('email')
     .notEmpty().withMessage('El campo no puede estar vacío').bail()
     .isEmail().withMessage('Debe ser un correo con formato válido').bail()
@@ -37,10 +45,19 @@ module.exports = [
           })
   }),
   body('password')
-    .notEmpty().withMessage('El campo no puede estar vacío').bail(),
-    body('checkbox')
+  .notEmpty().withMessage('El campo no puede estar vacío').bail()
+    .isLength({ min: 8, max: 20 }).withMessage("El valor ingresado debe tener al menos 8 caracteres y máximo 20").bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&.,;'" ])[A-Za-z\d$@$!%*?&.,;'" ]{6,20}$/).withMessage("Debes incluir números, mayúscula, minúscula y  un caracter especial($!%*?&.,;')").bail(),
+  body('checkbox')
     .custom(value => {
       return value === 'on';
     })
     .withMessage('Debe aceptar los términos y condiciones'),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        delete req.body.password;
+      }
+      next();
+    }
 ];
