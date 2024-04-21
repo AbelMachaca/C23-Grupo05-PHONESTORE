9c9f3d18fa6f63e567f2ece669f932c4544a77ee
EmmanuelCrocianelli <maguiteamoyoema@gmail.com>
2024-04-20 22:13:08 -0300

    se corrigieron las validaciones del front del formulario de login y de crear producto

--- phonestore_2.0/public/js/validacion-crear-producto.js
+++ phonestore_2.0/public/js/validacion-crear-producto.js
@@ -1,147 +1,72 @@
-const form = document.getElementById("form");
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaa");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Evitar que el formulario se envíe automáticamente
-
-  // Validar los campos del formulario uno por uno
-  const modelo = document.querySelector("#modelo").value;
-  const image = document.querySelector("#image").value;
-  const marca = document.querySelector("#marca").value;
-  const price = document.querySelector("#price").value;
-  const description = document.querySelector("#description").value;
-  const almacenamiento = document.querySelector("#almacenamiento").value;
-  const ram = document.querySelector("#ram").value;
-  const so = document.querySelector("#so").value;
-
-  let isValid = true;
-
-  // Validar nombre del modelo
-  if (modelo.length < 5) {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "El nombre del modelo debe tener al menos 5 caracteres.",
-    });
-    return; // Detener la ejecución aquí si el campo no es válido
-  }
-
-  // Validar las imágenes
-  if (image === "") {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "Debe seleccionar una imagen.",
-    });
-    return;
-  } else {
-    // Expresión regular para verificar la extensión del archivo
-    const allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-    if (!allowedExtensions.exec(image)) {
-      isValid = false;
-      Swal.fire({
-        icon: "error",
-        title: "Error",
-        text: "El archivo de imagen debe ser JPG, JPEG, PNG o GIF.",
-      });
-      return;
-    }
-  }
-
-  // Validar la marca
-  if (marca === "") {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "Debe seleccionar una marca.",
-    });
-    return;
-  }
-
-  // Validar el precio
-if (price === "") {
-  isValid = false;
-  Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "El campo precio no puede estar vacío.",
-  });
-  return;
-} else if (isNaN(price) || parseFloat(price) <= 0) {
-  isValid = false;
-  Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "El precio debe ser un número mayor que cero.",
-  });
-  return;
+// Función para limpiar el mensaje de error del campo de imagen cuando se selecciona una imagen
+function limpiarMensajeErrorImagen() {
+  const errorMessage = document.querySelector("#image").nextElementSibling; // Elemento que muestra el mensaje de error
+  errorMessage.textContent = ""; // Limpiar el mensaje de error
 }
 
+// Event listener para limpiar el mensaje de error del campo de imagen cuando se selecciona una imagen
+document.querySelector("#image").addEventListener("change", function() {
+  limpiarMensajeErrorImagen();
+});
 
-  // Validar la descripción
-  if (description.length < 20) {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "La descripción debe tener al menos 20 caracteres.",
-    });
-    return;
-  }
 
-  // Validar el almacenamiento
-  if (almacenamiento <= 0 || isNaN(almacenamiento)) {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "El almacenamiento debe ser un número mayor que cero.",
-    });
-    return;
-  }
+// Función para limpiar el mensaje de error cuando se comienza a llenar el campo
+function limpiarMensajeError(input) {
+  const errorMessage = input.nextElementSibling; // Elemento que muestra el mensaje de error
+  errorMessage.textContent = ""; // Limpiar el mensaje de error
+}
 
-  // Validar la RAM
-  if (ram <= 0 || isNaN(ram)) {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "La RAM debe ser un número mayor que cero.",
-    });
-    return;
-  }
+// Event listeners para limpiar el mensaje de error al comenzar a llenar un campo
+document.querySelectorAll('input[type="text"], input[type="number"], textarea, select').forEach(input => {
+  input.addEventListener("input", function() {
+      limpiarMensajeError(input);
+  });
+});
 
-  // Validar el sistema operativo
-  if (so === "") {
-    isValid = false;
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "Debe ingresar un sistema operativo.",
-    });
-    return;
-  }
+// Función para validar todos los campos
+function validarFormulario() {
+  let isValid = true;
 
-  // Si todos los campos son válidos, enviar el formulario
-  if (isValid) {
-    Swal.fire({
-      title: "¿Deseas guardar los cambios?",
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Guardar",
-      denyButtonText: "No guardar"
-    }).then((result) => {
-      if (result.isConfirmed) {
-        form.submit(); // Enviar el formulario si se confirma
-        Swal.fire("¡Guardado!", "", "success");
-      } else if (result.isDenied) {
-        // No hacer nada si se niega
-        Swal.fire("Los cambios no se guardaron", "", "info");
+  // Definir los campos y sus respectivos mensajes de error
+  const campos = [
+      { campo: "modelo", mensajeError: "El campo Modelo es obligatorio." },
+      { campo: "image", mensajeError: "Debe seleccionar una imagen." },
+      { campo: "marca", mensajeError: "Debe seleccionar una marca." },
+      { campo: "price", mensajeError: "El campo Precio es obligatorio." },
+      { campo: "description", mensajeError: "El campo Descripción es obligatorio." },
+      { campo: "almacenamiento", mensajeError: "El campo Almacenamiento es obligatorio." },
+      { campo: "ram", mensajeError: "El campo RAM es obligatorio." },
+      { campo: "so", mensajeError: "El campo Sistema Operativo es obligatorio." }
+  ];
+
+  // Iterar por cada campo y verificar si está vacío
+  campos.forEach(item => {
+      const valor = document.querySelector(`#${item.campo}`).value.trim();
+      const errorMessage = document.querySelector(`#${item.campo}`).nextElementSibling; // Elemento siguiente que mostrará el mensaje de error
+
+      // Verificar si el campo está vacío
+      if (valor === "") {
+          errorMessage.textContent = item.mensajeError;
+          isValid = false;
       }
-    });
+  });
+
+  return isValid;
+}
+
+// Event listener para el envío del formulario
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+
+  // Validar el formulario antes de enviarlo
+  const formularioValido = validarFormulario();
+
+  // Si el formulario es válido, enviarlo
+  if (formularioValido) {
+      // Aquí puedes agregar el código para enviar el formulario
+      this.submit();
+      console.log("El formulario es válido. Enviando...");
+  } else {
+      console.log("El formulario no es válido. Por favor, complete todos los campos.");
   }
-  
 });
