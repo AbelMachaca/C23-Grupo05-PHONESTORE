5875a0b06d925f57946c7bc015e7334e89d15574
EmmanuelCrocianelli <maguiteamoyoema@gmail.com>
2024-06-14 15:00:10 -0300

    actualizando

--- phonestore_2.0/src/database/config/config.js
+++ phonestore_2.0/src/database/config/config.js
@@ -1,26 +1,28 @@
 require('dotenv').config();
 
-module.exports= {
+module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database":process.env.DB_NAME,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT,
     "port": process.env.DB_PORT
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "database_test",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "dialect": process.env.DB_DIALECT || "mysql",
+    "port": process.env.DB_PORT || 3306
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "database_production",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "dialect": process.env.DB_DIALECT || "mysql",
+    "port": process.env.DB_PORT || 3306
   }
 }
